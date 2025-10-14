329f34a6bfffe558e67a87f0eb0b664cb3078441
Gleb <upingnev@gmail.com>
2025-10-14 10:00:43 +0700

    fix enter

--- g.neverov/lab4/lab4.c
+++ g.neverov/lab4/lab4.c
@@ -15,35 +15,26 @@ typedef struct Node
 
 Node* add_node(Node* prev, char* string)
 {
-    Node* ans;
-    ans = malloc(sizeof(Node));
+    Node* ans = malloc(sizeof(Node));
     if (ans == NULL)
-    {
         return NULL;
-    }
+    
     if (prev != NULL)
-    {
         prev->next = ans;
-    }
-
+    
     ans->next = NULL;
     ans->string = string;
     return ans;
 }
-
 
 void print_sanitized(const char *s)
 {
     for (const unsigned char *p = (const unsigned char *)s; *p != '\0'; p++)
     {
         if (isprint(*p))
-        {
             putchar(*p);
-        }
         else
-        {
             printf("\\x%02X", *p);
-        }
     }
     putchar('\n');
 }
@@ -62,6 +53,7 @@ void free_list(Node* cur)
     if (cur != NULL)
     {
         free_list(cur->next);
+        free(cur->string);
         free(cur);
     }
 }
@@ -69,28 +61,39 @@ void free_list(Node* cur)
 void exit_program(int code, char* message)
 {
     if (message != NULL)
-    {
         perror(message);
-    }
     exit(code);
 }
 
 int get_list_from_user(Node** startList)
 {
-    char buffer[LEN_BUFFER], flag_fullstop = 0, flag_start = 1;
+    char buffer[LEN_BUFFER];
     char* string = NULL;
     Node *prev = NULL, *start = NULL;
+    size_t string_len = 0;
 
     while (fgets(buffer, LEN_BUFFER, stdin))
     {
+        // Удаляем символ новой строки если он есть
+        size_t buf_len = strlen(buffer);
+        if (buf_len > 0 && buffer[buf_len - 1] == '\n')
+        {
+            buffer[buf_len - 1] = '\0';
+            buf_len--;
+        }
+
+        // Проверяем пустую строку
+        if (buf_len == 0)
+            continue;
+
+        // Проверяем на точку в начале строки
         if (buffer[0] == '.')
         {
-            if (strlen(buffer) <= 2)
-            {
+            if (buf_len == 1) // Только точка
                 break;
-            }
-            string = strdup(&buffer[1]);
-            flag_fullstop = 1;
+            
+            // Точка с текстом - копируем без точки
+            string = strdup(buffer + 1);
         }
         else
         {
@@ -98,56 +101,41 @@ int get_list_from_user(Node** startList)
         }
 
         if (string == NULL)
-        {
             return 1;
-        }
 
-        while (buffer[strlen(buffer) - 1] != '\n')
+        // Дополняем строку пока не достигнем конца ввода
+        while (buf_len == LEN_BUFFER - 1 && buffer[LEN_BUFFER - 2] != '\0')
         {
-            fgets(buffer, LEN_BUFFER, stdin);
-            if (ferror(stdin))
+            if (!fgets(buffer, LEN_BUFFER, stdin))
+                break;
+
+            buf_len = strlen(buffer);
+            if (buf_len > 0 && buffer[buf_len - 1] == '\n')
             {
-                return 1;
+                buffer[buf_len - 1] = '\0';
+                buf_len--;
             }
 
-            string = realloc(string, strlen(buffer) + strlen(string) + 1);
-            if (string == NULL)
+            char* new_string = realloc(string, strlen(string) + buf_len + 1);
+            if (new_string == NULL)
             {
+                free(string);
                 return 1;
             }
-
+            string = new_string;
             strcat(string, buffer);
         }
 
         prev = add_node(prev, string);
-
         if (prev == NULL)
-        {
             return 1;
-        }
 
-        if (flag_start)
-        {
-            flag_start = 0;
+        if (start == NULL)
             start = prev;
-        }
-
-        if (flag_fullstop)
-        {
-            break;
-        }
     }
 
     if (ferror(stdin))
-    {
         return 1;
-    }
-
-    if (feof(stdin))
-    {
-        return 2;
-    }
-
 
     *startList = start;
     return 0;
@@ -156,9 +144,8 @@ int get_list_from_user(Node** startList)
 int main()
 {
     Node* start = NULL;
-    Node** startPointer = &start;
 
-    switch (get_list_from_user(startPointer))
+    switch (get_list_from_user(&start))
     {
     case 1:
         exit_program(EXIT_FAILURE, "get_list_from_user failed");
@@ -170,7 +157,6 @@ int main()
 
     puts("strings:");
     print_list(start);
-
     free_list(start);
 
     exit_program(EXIT_SUCCESS, NULL);
