01a9ce4aa498708c2db93c8a45333dc4009c38d1
Gleb <upingnev@gmail.com>
2025-11-11 11:27:30 +0700

    new

--- g.neverov/lab17/lab17.c
+++ g.neverov/lab17/lab17.c
@@ -26,6 +26,7 @@ int main() {
     char input_string[41] = {0};
     int input_len = 0;
     char ch;
+    int last_space = -1;
 
     while (1) {
         ssize_t n = read(STDIN_FILENO, &ch, 1);
@@ -39,6 +40,16 @@ int main() {
                     input_len--;
                     write(STDOUT_FILENO, "\b \b", 3);
                     input_string[input_len] = '\0';
+                
+                    if (input_len == last_space) {
+                        last_space = -1;
+                        for (int i = input_len - 1; i >= 0; i--) {
+                            if (input_string[i] == ' ') {
+                                last_space = i;
+                                break;
+                            }
+                        }
+                    }
                 }
                 break;
             case 21: // KILL
@@ -47,6 +58,7 @@ int main() {
                     input_len--;
                 }
                 input_string[0] = '\0';
+                last_space = -1;
                 break;
             case 4: // CTRL+D
                 if (input_len == 0) {
@@ -65,29 +77,77 @@ int main() {
                         input_len--;
                     }
                     input_string[input_len] = '\0';
+                    last_space = -1;
+                    for (int i = input_len - 1; i >= 0; i--) {
+                        if (input_string[i] == ' ') {
+                            last_space = i;
+                            break;
+                        }
+                    }
                 }
                 break;
-            case 10: // \n
+            case 10:
                 input_string[input_len] = '\n';
                 input_string[input_len + 1] = '\0';
                 write(STDOUT_FILENO, "\n", 1);
                 input_len = 0;
+                last_space = -1;
                 break;
             default:     
                 if (ch < 32 || ch == 127) {
-                    write(STDOUT_FILENO, "\a", 1); // BELL
-                    write(STDOUT_FILENO, "^G", 2); // CTRL+G
+                    write(STDOUT_FILENO, "\a", 1);
+                    write(STDOUT_FILENO, "^G", 2);
                 } else {
                     if (input_len < 40) {
                         input_string[input_len++] = ch;
                         write(STDOUT_FILENO, &ch, 1);
+                        
+                        if (ch == ' ') {
+                            last_space = input_len - 1;
+                        }
                     } else {
-                        input_string[input_len] = '\n';
-                        input_string[input_len + 1] = '\0';
-                        write(STDOUT_FILENO, "\n", 1);
-                        write(STDOUT_FILENO, &ch, 1);
-                        input_len = 1;
-                        input_string[0] = ch;
+                        if (ch == ' ') {
+                            input_string[input_len] = '\n';
+                            input_string[input_len + 1] = '\0';
+                            write(STDOUT_FILENO, "\n", 1);
+                            write(STDOUT_FILENO, &ch, 1);
+                            input_len = 1;
+                            input_string[0] = ch;
+                            last_space = 0;
+                        } else {
+                            if (last_space != -1) {
+                                int chars_to_move = input_len - last_space - 1;
+                                
+                                write(STDOUT_FILENO, "\n", 1);
+                                
+                                for (int i = last_space + 1; i < input_len; i++) {
+                                    write(STDOUT_FILENO, &input_string[i], 1);
+                                }
+                                
+                                write(STDOUT_FILENO, &ch, 1);
+                                
+                                for (int i = 0; i < chars_to_move; i++) {
+                                    input_string[i] = input_string[last_space + 1 + i];
+                                }
+                                input_string[chars_to_move] = ch;
+                                input_len = chars_to_move + 1;
+                                
+                                last_space = -1;
+                                for (int i = 0; i < input_len; i++) {
+                                    if (input_string[i] == ' ') {
+                                        last_space = i;
+                                    }
+                                }
+                            } else {
+                                input_string[input_len] = '\n';
+                                input_string[input_len + 1] = '\0';
+                                write(STDOUT_FILENO, "\n", 1);
+                                write(STDOUT_FILENO, &ch, 1);
+                                input_len = 1;
+                                input_string[0] = ch;
+                                last_space = -1;
+                            }
+                        }
                     }
                 }
                 break;
